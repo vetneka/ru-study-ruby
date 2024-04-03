e55b1770d295db21e46d6ddc1b327cd3d54f6b8f
Andrei Akentev <akentev2010@gmail.com>
2024-04-03 09:45:24 +0500

    add solution for array methods

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,44 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        for item in self do
+          yield(item) if block_given?
+        end
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = my_reduce([]) { |acc, item| acc << yield(item) }
+
+        MyArray.new(result)
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = my_reduce([]) do |acc, item|
+          if item.nil?
+            acc
+          else
+            acc << item
+          end
+        end
+
+        MyArray.new(result)
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(*args)
+        array = args.empty? ? slice(1..) : self
+
+        accumulator = args.first || first
+
+        for item in array do
+          accumulator = yield(accumulator, item) if block_given?
+        end
+
+        accumulator
+      end
     end
   end
 end
