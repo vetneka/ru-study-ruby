1c237ef01430463751d11ff5c725d6e7a629e49a
Andrei Akentev <akentev2010@gmail.com>
2024-04-09 08:43:41 +0500

    fix review issues

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -4,7 +4,9 @@ module Exercise
       def max(array)
         max_value = array[0]
 
-        for item in array do
+        array.each_with_index do |item, index|
+          next if index.zero?
+
           max_value = item if item > max_value
         end
 
@@ -13,35 +15,26 @@ module Exercise
 
       def replace(array)
         max_value = max array
-        result = []
 
-        for item in array do
-          result << (item.positive? ? max_value : item)
-        end
-
-        result
+        array.map { |item| item.positive? ? max_value : item }
       end
 
-      def search_iterate(array, query, left_index, rigth_index)
-        return -1 if left_index > rigth_index
+      def search_iterate(array, query, left_index = 0, right_index = array.length - 1)
+        query_exist_in_array = query >= array[left_index].to_f && query <= array[right_index].to_f
+        left_index_more_then_right_index = left_index > right_index
 
-        middle_index = (left_index + rigth_index) / 2
+        return -1 if !query_exist_in_array || left_index_more_then_right_index
+
+        middle_index = (left_index + right_index) / 2
         current_value = array[middle_index]
 
-        if current_value == query
-          middle_index
-        elsif current_value < query
-          search_iterate(array, query, middle_index + 1, rigth_index)
-        else
-          search_iterate(array, query, left_index, middle_index - 1)
-        end
+        return middle_index if current_value == query
+
+        current_value < query ? search_iterate(array, query, middle_index + 1, right_index) : search_iterate(array, query, left_index, middle_index - 1)
       end
 
       def search(array, query)
-        left_index = 0
-        right_index = _array.length - 1
-
-        search_iterate(array, query, left_index, right_index)
+        search_iterate(array, query)
       end
     end
   end
